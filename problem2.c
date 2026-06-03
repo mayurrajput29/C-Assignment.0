2c7dd027a51ae935febdff9d79f4632648b95ce8
Mayur Rajput <mayurrajput80314@gmail.com>
2026-06-03 23:21:53 +0530

    C-Assignment.0
    C-Assignment.0 - This is my c assignment question solved program and 
    2nd question completed

---
+++ problem2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+//Q.Write a C program to find the area of a circle.
+//-----------------------------------------------------------
+
+
+float radius;
+
+printf("The radius is :");
+scanf("%f",&radius);
+
+printf("The circle radius is :%f",3.14*radius*radius);
+
+
+    return 0;
+}
