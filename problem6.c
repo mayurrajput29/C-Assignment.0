1263544283c634ff0aeb1a774b007775a9b23d5b
Mayur Rajput <mayurrajput80314@gmail.com>
2026-06-07 16:10:17 +0530

    C-Assignment0
     Write a C program to find the square and cube of a given number. #problem6.c

---
+++ problem6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+// Write a C program to find the square and cube of a given number //#problem6.c
+
+int area;
+int cube;
+
+printf("Enter the Area of square :");
+scanf("%d",&area);
+printf("%d",area=area*area);
+
+printf("The volume of cube is :");
+scanf("%d",&cube);
+printf("%d",cube=cube*cube*cube);
+
+
+  return 0;
+}
